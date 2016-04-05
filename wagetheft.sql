9d59544c022b98fdd51f6e8baa015e01e529bf16
TomRigg <tomriggia@gmail.com>
2016-04-05 10:18:37 -0500

    Update wagetheft.sql

--- wagetheft.sql
+++ wagetheft.sql
@@ -1,38 +1,59 @@
--- Get rid of any old data by resetting the table
-DROP TABLE IF EXISTS Employee;
-DROP TABLE IF EXISTS Employer;
-DROP TABLE IF EXISTS Jobs;
-DROP TABLE IF EXISTS Hours;
+
+DROP TABLE IF EXISTS Employee CASCADE CONSTRAINTS;
+DROP TABLE IF EXISTS Employer CASCADE CONSTRAINTS;
+DROP TABLE IF EXISTS Jobs CASCADE CONSTRAINTS;
+DROP TABLE IF EXISTS Hours CASCADE CONSTRAINTS;
+DROP TABLE IF EXISTS Paycheck CASCADE CONSTRAINTS;
+
 
 CREATE TABLE Employee (
-  EmployeeID int unsigned NOT NULL AUTO_INCREMENT,
-  name int NOT NULL,
-  Phone Number varchar(128) DEFAULT NULL,
-  Address varchar(18) NOT NULL,
-  PRIMARY KEY (EmployeeID)
-  FOREIGN KEY (JobID) REFERENCES Jobs(JobID),
-) ENGINE=InnoDB;
+  employeeID INT AUTO_INCREMENT,
+  jobID INT,
+  name VARCHAR(40) NOT NULL,
+  phoneNumber VARCHAR(15) DEFAULT NULL,
+  employeeAddress VARCHAR(80) NOT NULL,
+  PRIMARY KEY(EmployeeID)
+)
 
 CREATE TABLE Employer (
-  EmployerID int unsigned NOT NULL AUTO_INCREMENT,
-  Address VARCHAR (40) 
-  PRIMARY KEY (EmployerID)
+  employerID INT AUTO_INCREMENT,
+  employerAddress VARCHAR(40), 
+  PRIMARY KEY(EmployerID)
 )
 
 CREATE TABLE Jobs (
-  JobID int unsigned NOT NULL AUTO_INCREMENT,
-  Work Address VARCHAR (40), 
-  PRIMARY KEY (JobID),
-  FOREIGN KEY (EmployerID) REFERENCES Employer(EmployerID),
-  FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID),
-) 
+  jobID INT AUTO_INCREMENT,
+  employerID INT,
+  employeeID INT,
+  hourlyWage DECIMAL(6,2),
+  workAddress VARCHAR(80),
+  PRIMARY KEY(JobID),
+  FOREIGN KEY(employerID) REFERENCES Employer(employerID),
+  FOREIGN KEY(employeeID) REFERENCES Employee(employeeID) 
+)
+
 
 CREATE TABLE Hours (
-  HoursID int unsigned NOT NULL,
-  Date VARCHAR (40),
-  Work Address VARCHAR (40), 
-  Hours Worked int (3),
-  WagesEarned int (6),
+  hoursID INT unsigned NOT NULL,
+  jobID INT,
+  date VARCHAR(40),
+  workAddress VARCHAR(80), 
+  hoursWorked INT(3),
+  wagesEarned INT(6),
   PRIMARY KEY (HoursID),
-  FOREIGN KEY (JobID) REFERENCES Jobs(JobID),
+  FOREIGN KEY (JobID) REFERENCES Jobs(JobID)
 ) 
+
+CREATE TABLE Paycheck (
+  paycheckID int unsigned NOT NULL,
+  jobID INT,
+  checkDate VARCHAR(20),
+  hoursWorked INT(3),
+  wagesEarned INT(6),
+  PRIMARY KEY (paycheckID),
+  FOREIGN KEY (JobID) REFERENCES Jobs(JobID)
+) 
+  
+  
+ALTER TABLE Employee ADD CONSTRAINT FOREIGN KEY (jobID) REFERENCES Jobs(JobID);
+
